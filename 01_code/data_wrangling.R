443be78e678a59a26f60137f3f0d3f13726fd3d3
jens-klenke <57100601+jens-klenke@users.noreply.github.com>
2020-10-13 13:39:34 +0200

    convergence plot

--- 01_code/data_wrangling.R
+++ 01_code/data_wrangling.R
@@ -3,15 +3,37 @@ source(here::here('01_code/packages.R'))
 ## read data
 co2_data <- read_csv(here::here('00_data/owid-co2-data.csv'))
 
+# codebook
+# codebook <- read_csv(here::here('00_data/owid-co2-codebook.csv'))
+
 ## data overview 
 names(co2_data)
 str(co2_data)
 des <- Hmisc::describe(co2_data)
 
-## data after 1945 
+### G28 countries 
+# 
 
-co2_data_45 <- co2_data%>%
-  dplyr::filter(year > 1945)
+G_28 <- c('Australia', 'Austria', 'Belgium', 'Canada', 'Denmark', 'France', 'Finland', 'Germany', 'Italy', 'Japan', 'Netherlands', 'Spain', 'Sweden', 'Switzerland', 'United Kingdom', 'United States', 'Argentina', 'Brazil', 'Chile', 'China', 'Greece', 'India', 'Indonesia', 'Mexico', 'New Zealand', 'Peru', 'Portugal', 'Taiwan')
+
+## data after 1945 and 
+
+co2_data_45_g28 <- co2_data%>%
+  dplyr::filter(year > 1945,
+                country %in% G_28)%>%
+  dplyr::select(year, country, co2, population)%>%
+  dplyr::mutate(co2_per_capita = co2 / (population/1000000))%>%
+  dplyr::group_by(year)%>%
+  dplyr::mutate(mean_co2_per_capita = mean(co2_per_capita))%>%
+  dplyr::mutate(demeand_co2_per_capita = co2_per_capita - mean_co2_per_capita)
+
+
+ggplot(co2_data_45_g28, aes(x = year, y = demeand_co2_per_capita, colour = country))+
+  geom_line()
+
+
+
+
 
 
 
--- 01_code/packages.R
+++ 01_code/packages.R
@@ -1,4 +1,4 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(dplyr,
                readr,
-               hmisc)
+               Hmisc)
